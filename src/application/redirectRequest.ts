e95cec30df577a022df263b32e88d3f93e6179b8
zsolnai <georg.zsolnai123@gmail.com>
2024-10-28 20:53:12 +0100

    Change redirect to include targetId parameter

--- src/application/redirectRequest.ts
+++ src/application/redirectRequest.ts
@@ -1,17 +1,16 @@
 import { Request } from "../domain/request";
-import { Role } from "../domain/types";
 import { RequestRepository } from "../infrastructure/repositories/requestRepository";
 
 export class RedirectRequest {
 
     constructor(private readonly requestRepository: RequestRepository) {}
 
-    execute(userId: string, requestId: string, updates: Partial<Request>, role: Role): string{
+    execute(targetId: string, requestId: string): string{
        
         const requestToRedirect = this.requestRepository.getRequestById(requestId);
         if (!requestToRedirect) return "Request not found.";
         //const currReq = this.requests.splice(requestToRedirect, 1)[0]; 
-        const updatedRequest = this.requestRepository.updateRequest(requestId, new Request(requestId, requestToRedirect.clientId, userId, requestToRedirect.eventName, requestToRedirect.proposedBudget, requestToRedirect.staffRequirement, requestToRedirect.date, requestToRedirect.details, requestToRedirect.status))
+        const updatedRequest = this.requestRepository.updateRequest(requestId, new Request(requestId, requestToRedirect.clientId, targetId, requestToRedirect.eventName, requestToRedirect.proposedBudget, requestToRedirect.staffRequirement, requestToRedirect.date, requestToRedirect.details, requestToRedirect.status))
 
         if(!updatedRequest) return "Request not updated"
         return `Request ${requestId} has been redirected.`;
